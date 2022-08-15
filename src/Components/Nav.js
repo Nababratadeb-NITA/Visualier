41e116d1ee6474519b50068a925a23097a87b5fe
nababrata12@ <nababratadeb5@gmail.com>
2022-08-15 16:05:51 +0530

    first commit

---
+++ src/Components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { DiCode } from "react-icons/di";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="nav_logo">
+        Visualizer <DiCode className="nav__icon" />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
